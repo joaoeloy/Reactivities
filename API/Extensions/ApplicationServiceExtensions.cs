fba533ae09519259a5456c139615e96cbbe89a76
jmve <joao--eloy@hotmail.com>
2021-05-10 23:40:48 +0100

    end of section 4 - add CRUD functionallity

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,29 @@
+using Application.Activities;
+using Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Persistance;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
+            services.AddDbContext<DataContext>(options => {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
